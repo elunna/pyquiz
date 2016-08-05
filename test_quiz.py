c55d85f3ea1a31a0940fc08707db1e78a1d7c4ad
Erik Lunna <eslunna@gmail.com>
2016-08-05 11:44:19 -0400

    Added the outline for testing the quiz class and test .quiz files.

---
+++ test_quiz.py
@@ -0,0 +1,22 @@
+import unittest
+import quiz
+
+class TestQuiz(Unittest.testcase):
+    # Tests for import_database(db_file):
+
+    # Test importing a single question/answer pair.
+    # Test importing a question with multiple answers
+    # Test importing a variety of question/answers
+
+"""
+def display_database(test_dict):
+def guess(answer, question):
+def test_keys(user, test_dict):
+def test_values(test_dict):
+def is_valid_entry(termdef):
+def make_test_set(db, quantity):
+def choose_quiz():
+def process_user():
+def display_user(user):
+def save_user(user):
+"""
